69de09aa00f47f86993ea242df7945c7c8946303
Kenneth Endfinger <k.endfinger@dglogik.com>
2016-10-03 14:35:29 -0400

    Fix bugs in kernel cleaner and upstream rebaser.

--- clean-kernels.sh
+++ clean-kernels.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+source "$(dirname $0)/common.sh"
+
 CURRENT=$(uname -r)
 BUILT=$(strings ${KERNEL_DIR}/vmlinux | grep "Linux version" | awk '{print $3}')
 REMOVE=$(ls /lib/modules | grep -v -e "${CURRENT}" -e "${BUILT}" -e "extramodules" | grep "\-DC" || true)
--- rebase-upstream.sh
+++ rebase-upstream.sh
@@ -3,6 +3,8 @@ set -e
 
 source "$(dirname $0)/common.sh"
 
+cd ${KERNEL_DIR}
+
 git fetch --all
 git checkout master
 git reset --hard linus/master
